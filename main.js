b9005651e32446442f115e2857ca2c7d9877f166
John Do <jdo.dev45@gmail.com>
2021-09-22 18:32:03 -0700

    style updated to show max text entered

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var charEntered = document.getElementById('char-entered')
 var charRemaining = document.getElementById('char-remaining')
+var charUpdate = document.getElementById('char-update')
 var textarea = document.querySelector('textarea')
 
 window.addEventListener('DOMContentLoaded', event => {
@@ -11,4 +12,12 @@ textarea.addEventListener('input', event => {
   let text = event.target.value
   charEntered.textContent = text.length
   charRemaining.textContent = 280 - text.length
+
+  if (text.length > 280) {
+    charUpdate.style.color = 'red'
+    textarea.style.border = '3px solid red'
+  } else {
+    charUpdate.style.color = '#1DA1F2'
+    textarea.style.border = '3px solid #1DA1F2'
+  }
 })
